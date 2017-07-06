6e49cc7810e48a28edf1e80fc72026664f1d4b04
Tania Filonenko <tetiana.filonenko2012@gmail.com>
2017-07-06 11:49:55 +0300

    banner with corner prepare

--- app/assets/javascript/components/dashboard/home_page.vue
+++ app/assets/javascript/components/dashboard/home_page.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="content">
       <div class="main-page-banner-container">
-        <div class="main-page-banner-container__square"></div>
+        <div class="main-page-banner__image-container">
+          <div class="main-page-banner__image"></div>
+        </div>
+        <div class="main-page-banner__title-container">
+          <div class="main-page-banner__title">Dawien</div>
+          <div class="main-page-banner__description">Everyone has something to sell. The greatest thing you can ever sell is an idea or talent.</div>
+        </div>
       </div>
       <div class="page-container">
-        <div class="welcome-container"></div>
+        <div class="welcome-container">
+          <div  class="welcome-container__title">Welcome</div>
+          <div  class="welcome-container__page">ddddd</div>
+        </div>
         <div class="benefit-container"></div>
         <div class="statistic-container"></div>
         <div class="merchant-container"></div>
