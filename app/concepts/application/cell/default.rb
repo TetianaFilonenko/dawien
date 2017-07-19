c8d49a00ba48e037679242245ae84dc21d8f915a
tetiana.filonenko <tetiana.filonenko2012@gmail.com>
2017-07-19 13:31:08 +0300

    translate erb to haml

--- app/concepts/application/cell/default.rb
+++ app/concepts/application/cell/default.rb
@@ -1,10 +1,7 @@
-require 'cell/erb'
-
 module Application
   module Cell
     class Default < Trailblazer::Cell
       layout :default
-      include :Cell::Erb
       include ActionView::Helpers::CsrfHelper
       include Webpacker::Helper
     end
