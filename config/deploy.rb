c9e5e566d3229e037045f758e57db88a3e747f5c
tetiana.filonenko <tetiana.filonenko2012@gmail.com>
2017-07-19 16:43:33 +0300

    puma version

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,6 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, '2.4.1' # Edit this if you are using MRI Ruby
-
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
